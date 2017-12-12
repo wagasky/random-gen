d6881dcbb76e62bcef4d9ae6f3ae6dca73842873
Melissa Wagasky <melissawagasky@gmail.com>
2017-12-12 10:50:17 -0700

    Add random letter generator

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const generateRandomNumbers = require('./lib/gen-random-numbers.js');
+const generateRandomLetters = require('./lib/gen-random-letters.js');
+
 
 module.exports = {
   generateRandomNumbers
